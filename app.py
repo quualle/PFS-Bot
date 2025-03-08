93d1c4bf9944a671da4efda51fe47c4d20b114b7
Marco Heer <marcoheer@Laptop-von-Marco.local>
2025-03-08 08:38:28 +0100

    Test: Änderung der Überschrift in /test_bigquery

--- app.py
+++ app.py
@@ -943,7 +943,7 @@ def test_bigquery():
         <!DOCTYPE html>
         <html>
         <head>
-            <title>BigQuery Test Dashboard</title>
+            <title>BigQuery Dashboard zum Testen der geladenen Daten</title>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">

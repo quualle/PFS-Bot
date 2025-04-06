5b28d4525cad1b9a61a669c280c8c0b6939dc90b
quualle <marcoheer@live.de>
2025-04-06 11:40:53 +0200

    Kündihgungen in Meine KPIs befüllt- neue berechnung

--- app.py
+++ app.py
@@ -2880,6 +2880,8 @@ def get_kpi_data():
             query_name = "get_posting_contract_conversion"
         elif query_type == 'termination_rate':
             query_name = "get_contract_terminations"
+        elif query_type == 'contract_count':
+            query_name = "get_contract_count"
         else:  # Standardfall: Abschlussquote
             query_name = "get_cvr_lead_contract"
             
